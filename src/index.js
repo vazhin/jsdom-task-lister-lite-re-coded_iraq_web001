67202eb7f2711a34617d77046ec5631be5ba80f2
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-21 19:13:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const todoList = document.querySelector("#tasks");
   const taskText = document.querySelector("#new-task-description");
-  const taskText = document.querySelector("#submitButton");
+  const addTaskButton = document.querySelector("#submit-button");
 
 
 
