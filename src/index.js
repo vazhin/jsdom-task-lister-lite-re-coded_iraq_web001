77c1225b4d5246e5d6adf17cc7500b9e4b6bbdda
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-21 19:41:37 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const todoList = document.querySelector("#tasks");
+  const taskText = document.querySelector("#new-task-description");
+  const addTaskButton = document.querySelector("#submit-button");
+  const priority = document.querySelector("#select-priority");
+
+  addTaskButton.addEventListener('click', addTask);
+
+function addTask (event){
+  event.preventDefault();
+  const task = document.createElement("li");
+  const deleteBtn = document.createElement("button");
+
+  todoList.appendChild(task);
+  todoList.appendChild(deleteBtn);
+
+
+  if(priority.value === "1"){
+    task.style.color = "red";
+  }
+  else if(priority.value === "2"){
+    task.style.color = "yellow";
+  }
+  else if(priority.value === "3"){
+    task.style.color = "green";
+  }
+
+  task.innerText = `${taskText.value}`;
+  deleteBtn.innerText = `❌`;
+  taskText.value = "";
+
+  deleteBtn.addEventListener('click', function (event){
+    event.preventDefault();
+    task.remove();
+    deleteBtn.remove();
+  })
+
+}
+
 });
